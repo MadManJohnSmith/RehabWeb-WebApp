e3c405bc9b3d5cd6528af3b55df66f352d4ae224
dafne <162538733+daaaf18@users.noreply.github.com>
2026-05-18 01:13:09 -0600

    feat(sessions): mejorar formato de fecha a es-MX HU-05

--- src/app/workspace/pages/sessions/session-history-page.component.ts
+++ src/app/workspace/pages/sessions/session-history-page.component.ts
@@ -205,4 +205,13 @@ export class SessionHistoryPageComponent {
       this.viewReport(r);
     }
   }
+
+  formatDate(dateStr: string): string {
+  const date = new Date(dateStr);
+  return date.toLocaleDateString('es-MX', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  });
+}
 }
